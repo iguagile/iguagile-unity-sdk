8be2d4641e3c2dbdaeb4661d6a7a9c6c43a01f68
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-03-16 23:25:08 +0900

    change threshold to the square of the set value

--- iguagile/Assets/Scripts/Iguagile/IguagilePlayer.cs
+++ iguagile/Assets/Scripts/Iguagile/IguagilePlayer.cs
@@ -8,10 +8,12 @@ namespace Iguagile
     public class IguagilePlayer : IguagileBehaviour
     {
         public List<IguagileTracker> Trackers = new List<IguagileTracker>();
-        public float threshold;
+        public float Threshold;
+        private float thresholdSquare;
 
         void Start()
         {
+            UpdateThreshold(Threshold);
         }
 
         void Update()
@@ -23,6 +25,12 @@ namespace Iguagile
             SyncTrackers();
         }
 
+        public void UpdateThreshold(float threshold)
+        {
+            Threshold = threshold;
+            thresholdSquare = threshold * threshold;
+        }
+
         public void AddTracker(IguagileTracker tracker)
         {
             Trackers.Add(tracker);
@@ -31,7 +39,7 @@ namespace Iguagile
         public void SyncTrackers()
         {
             byte[] data = {(byte) RpcTargets.OtherClients, (byte) MessageTypes.Trackers};
-            var transforms = Trackers.Where(x => x.IsMove(threshold)).Select(x => new IguagileTransform(x.transform, x.TrackerType)).ToArray();
+            var transforms = Trackers.Where(x => x.IsMove(thresholdSquare)).Select(x => new IguagileTransform(x.transform, x.TrackerType)).ToArray();
             if (transforms.Length == 0)
             {
                 return;
