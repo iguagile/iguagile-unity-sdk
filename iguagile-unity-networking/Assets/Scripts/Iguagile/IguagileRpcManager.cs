06bd16b0393233c3545f6e63c90d0ae10d84557c
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-08-02 22:08:41 +0900

    Create IguagileRpcManager

---
+++ iguagile-unity-networking/Assets/Scripts/Iguagile/IguagileRpcManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Iguagile
+{
+    public class IguagileRpcManager
+    {
+        private static Dictionary<string, IguagileBehaviour> _rpcMethods = new Dictionary<string, IguagileBehaviour>();
+        
+        internal static void InvokeRpc(string methodName, object[] args)
+        {
+            if (!_rpcMethods.ContainsKey(methodName))
+            {
+                return;
+            }
+
+            var behaviour = _rpcMethods[methodName];
+            var type = behaviour.GetType();
+            var flag = BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance;
+            var method = type.GetMethod(methodName, flag);
+            method?.Invoke(behaviour, args);
+        }
+
+        internal static void AddRpc(string methodName, IguagileBehaviour receiver)
+        {
+            _rpcMethods.Add(methodName, receiver);
+        }
+
+        internal static void RemoveRpc(string methodName)
+        {
+            if (_rpcMethods.ContainsKey(methodName))
+            {
+                _rpcMethods.Remove(methodName);
+            }
+        }
+
+        internal static void Clear()
+        {
+            _rpcMethods.Clear();
+        }
+    }
+}
