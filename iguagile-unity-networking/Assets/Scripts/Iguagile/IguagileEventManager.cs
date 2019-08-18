5964e3303ff6c24079d337103be1259776ca7d66
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-08-18 19:37:34 +0900

    add IguagileEventManager

---
+++ iguagile-unity-networking/Assets/Scripts/Iguagile/IguagileEventManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Iguagile
+{
+    public class IguagileEventManager
+    {
+        public static event InstantiatedEventHandler Instantiated;
+
+        public static void InvokeInstantiated(int userId, IguagileView view)
+        {
+            Instantiated?.Invoke(userId, view);
+        }
+    }
+}
