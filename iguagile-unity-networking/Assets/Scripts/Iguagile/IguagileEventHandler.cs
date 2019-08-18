c02e53b123dca5e31e765db7f642b56f5122ec86
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-08-18 19:36:11 +0900

    add InstantiatedEventHandler

--- iguagile-unity-networking/Assets/Scripts/Iguagile/IguagileEventHandler.cs
+++ iguagile-unity-networking/Assets/Scripts/Iguagile/IguagileEventHandler.cs
@@ -1,5 +1,8 @@
-﻿namespace Iguagile
+﻿using UnityEngine;
+
+namespace Iguagile
 {
     public delegate void ConnectionEventHandler();
     public delegate void ReceivedEventHandler(byte[] message);
+    public delegate void InstantiatedEventHandler(int userId, IguagileView view);
 }
