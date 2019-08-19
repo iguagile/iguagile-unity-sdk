f9a6c3d874fb3620653f24dc4e8dbc04ee9399f4
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-08-19 19:58:03 +0900

    add MigrateHostEventHandler

--- iguagile-unity-networking/Assets/Scripts/Iguagile/IguagileEventHandler.cs
+++ iguagile-unity-networking/Assets/Scripts/Iguagile/IguagileEventHandler.cs
@@ -5,4 +5,5 @@ namespace Iguagile
     public delegate void ConnectionEventHandler();
     public delegate void ReceivedEventHandler(byte[] message);
     public delegate void InstantiatedEventHandler(int userId, IguagileView view);
+    public delegate void MigrateHostEventHandler();
 }
